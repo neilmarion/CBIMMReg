6b52548b13461bdcd9ff443d7711439c42b91e1c
neilmarion <nmfdelacruz@gmail.com>
2011-07-26 19:06:00 +0800

    deleted migration

--- db/migrate/20110726110021_add_is_deleted_to_mobile_wallpapers.rb
+++
@@ -1,9 +0,0 @@
-class AddIsDeletedToMobileWallpapers < ActiveRecord::Migration
-  def self.up
-    add_column :mobile_wallpapers, :is_deleted, :boolean
-  end
-
-  def self.down
-    remove_column :mobile_wallpapers, :is_deleted
-  end
-end
